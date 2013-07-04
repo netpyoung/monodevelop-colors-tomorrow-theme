a68e2f8bae35cfc9343fa01fdc8974a848b11e39
netpyoung <netpyoung@gmail.com>
2013-07-04 14:02:09 +0900

    add Test.cs

---
+++ Test.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour {
+	
+	int A { get; set; }
+	
+	public struct SomeStruct {
+		public int x;
+		
+		public static SomeStruct operator+(SomeStruct s, int a) {
+			return s;
+		}
+	}
+	
+	void Start () {
+		
+		// this is normal comment
+		
+		/// this is doc comment
+		
+		var x = "this is string";
+		
+		this.A = -1234;
+		
+		if (true) { }
+		
+		for (;;) { }
+		
+		foreach (char c in x) {	}
+		
+		switch (0) { default: break; }
+		
+		try { } catch { }
+		
+		return;
+	}
+}
